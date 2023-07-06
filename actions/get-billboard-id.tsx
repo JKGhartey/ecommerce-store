803aaaab7c0343bf1ac431ad524b96ac9c645c91
Jerome Ghartey <gharteyjerome@gmail.com>
2023-07-06 06:31:44 +0100

    billboardID

--- actions/get-billboard-id.tsx
+++ actions/get-billboard-id.tsx
@@ -5,7 +5,7 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 const getBillboardId = async (): Promise<string> => {
   const response = await fetch(URL);
   const data = await response.json();
-  const billboardId = data[1].id; // Assuming the ID property is called 'id' in the API response
+  const billboardId = data[0].id; // Assuming the ID property is called 'id' in the API response
 //   const res = await fetch(`${URL}/${id}`);
   return billboardId
   
